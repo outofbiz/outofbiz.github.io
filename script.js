19d58a3761d8b451cfd8cfde3301f530222c5323
Charles Zahn <charlesazahn@gmail.com>
2017-01-23 21:58:16 -0500

    use relative paths

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ var background = document.getElementById('background')r
 window.addEventListener('click', function(e) {
   var image = document.createElement('img');
   var style = 'margin-top:' + e.screenY + 'px;margin-left:' + e.screenX + 'px';
-  image.setAttribute('src', '/digging.gif');
+  image.setAttribute('src', './digging.gif');
   image.setAttribute('style', style);
   background.prepend(image);
 });
